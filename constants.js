9954bbbc32e90572cd25523a9b91872706e0d116
MuhammadBabarWaseem <mbabarwaseem@gmail.com>
2024-09-15 14:39:36 +0500

    feat: added portfolio

---
+++ constants.js
@@ -0,0 +1,300 @@
+export const METADATA = {
+  author: "Syed Shahroze Rizvi",
+  title: "Portfolio | Syed Shahroze Rizvi",
+  description:
+    "Syed Shahroze Rizvi is a passionate Full Stack Engineer, dedicated to crafting aesthetic and modern apps that captivate and engage users.",
+  siteUrl: "https://mbabarwaseem.vercel.app/",
+  keywords: [
+    "Syed Shahroze Rizvi",
+    "Frontend Engineer",
+    "Full Stack Developer",
+    "React Native Developer",
+    "Software Engineer",
+    "Portfolio",
+    "Devfolio",
+    "Folio",
+  ].join(", "),
+  language: "English",
+  themeColor: "#000000",
+};
+
+export const MENULINKS = [
+  {
+    name: "Home",
+    ref: "home",
+  },
+  {
+    name: "Skills",
+    ref: "skills",
+  },
+  {
+    name: "Projects",
+    ref: "projects",
+  },
+  {
+    name: "Work",
+    ref: "work",
+  },
+  {
+    name: "Contact",
+    ref: "contact",
+  },
+];
+
+export const TYPED_STRINGS = [
+  "I create aesthetic and modern apps",
+  "A skilled Full Stack Developer",
+  "Crafting seamless user experiences",
+  "Passionate about cutting-edge technologies",
+  "Transforming ideas into digital solutions",
+];
+
+export const SOCIAL_LINKS = [
+  {
+    name: "mail",
+    url: "mailto: mbabarwaseem@gmail.com",
+  },
+  {
+    name: "linkedin",
+    url: "https://www.linkedin.com/in/muhammadbabarwaseem/",
+  },
+  {
+    name: "github",
+    url: "https://github.com/muhammadbabarwaseem",
+  },
+  {
+    name: "instagram",
+    url: "https://www.instagram.com/muhammadbabarwaseem/",
+  },
+];
+
+export const SKILLS = {
+  languagesAndTools: [
+    "html",
+    "css",
+    "graphql",
+    "javascript",
+    "typescript",
+    "sass",
+    "nodejs",
+    "vite",
+    "pusher",
+    "firebase",
+    "amplify",
+    "tensorflow",
+    "tanstack-query",
+  ],
+  librariesAndFrameworks: [
+    "react",
+    "nestjs",
+    "nextjs",
+    "tailwindcss",
+    "chakra-ui",
+  ],
+  databases: ["mysql", "postgresql", "mongodb", "redis"],
+  other: ["git", "sanity-io"],
+};
+
+export const PROJECTS = [
+  {
+    name: "E Menu System",
+    image: "/projects/EMenuSystem.png",
+    blurImage: "/projects/blur/airbnb-blur.webp",
+    description: "Advance QR Based E Menu Project 🚀",
+    gradient: ["#662d8c", "#ed1e79"],
+    url: "https://fyp-4pfn.onrender.com/",
+    tech: ["mongodb", "expressjs", "react", "nodejs", "tailwindcss", "shadcn"],
+  },
+  {
+    name: "Swift Stack",
+    image: "/projects/SwiftStack.png",
+    blurImage: "/projects/blur/medium-blur.webp",
+    description: "A Full Stack Web Application for Deployments 🚀",
+    gradient: ["#5efce8", "#736efe"],
+    url: "https://www.linkedin.com/feed/update/urn:li:activity:7167076640944996353/",
+    tech: [
+      "mongodb",
+      "expressjs",
+      "react",
+      "nodejs",
+      "redis",
+      "amplify",
+      "shadcn",
+    ],
+  },
+  {
+    name: "Chat App",
+    image: "/projects/ChatApp.png",
+    blurImage: "/projects/blur/medium-blur.webp",
+    description: "Advance and Scalable Chat App Using AES Encryption 🚀",
+    gradient: ["#F14658", "#DC2537"],
+    url: "https://aes-chat-app.vercel.app/",
+    tech: ["nextjs", "tailwindcss", "redis", "pusher"],
+  },
+  {
+    name: "Hotels By Day",
+    image: "/projects/HBD.png",
+    blurImage: "/projects/blur/tesla-blur.webp",
+    description: "A React Native App for Booking Hotels 🚀",
+    gradient: ["#62cff4", "#2c67f2"],
+    url: "https://play.google.com/store/apps/details?id=com.hotelsbyday.app&hl=en&pli=1",
+    tech: ["react", "firebase", "javascript", "css"],
+  },
+  {
+    name: "Event App (Mobile)",
+    image: "/projects/EventApp.jpg",
+    blurImage: "/projects/blur/medium-blur.webp",
+    description: "A React Native App for Event Management 🚀",
+    gradient: ["#0c0014", "#117aca"],
+    url: "/",
+    tech: ["react", "typescript", "graphql", "amplify", "tanstack-query"],
+  },
+  {
+    name: "Event App (Web)",
+    image: "/projects/EventWeb.png",
+    blurImage: "/projects/blur/medium-blur.webp",
+    description: "A FullStack WebApp For Event Management 🚀",
+    gradient: ["#2e4fc6", "#ed4182"],
+    url: "http://tehirapuao.io/",
+    tech: [
+      "react",
+      "typescript",
+      "graphql",
+      "amplify",
+      "tanstack-query",
+      "tailwindcss",
+    ],
+  },
+  {
+    name: "AllHouse",
+    image: "/projects/AllHouse.png",
+    blurImage: "/projects/blur/tesla-blur.webp",
+    description: "AllHouse Estate Agent App 🚀",
+    gradient: ["#3cc5d7", "#47d794"],
+    url: "/",
+    tech: ["react", "graphql", "typescript", "firebase", "redux", "css"],
+  },
+  {
+    name: "Pound App",
+    image: "/projects/Pound.png",
+    blurImage: "/projects/blur/airbnb-blur.webp",
+    description: "A Pound Character Game App UI 🚀",
+    gradient: ["#233329", "#63d471"],
+    url: "/",
+    tech: ["react", "typescript", "redux", "css"],
+  },
+  {
+    name: "Zindabhag",
+    image: "/projects/ZB.jpeg",
+    blurImage: "/projects/blur/medium-blur.webp",
+    description: "Zindabhag WebApp Using MERN Stack 📰",
+    gradient: ["#FFA62E", "#EA4D2C"],
+    url: "https://zindabhag.com/",
+    tech: ["typescript", "react", "nodejs", "tailwindcss", "mysql"],
+  },
+  {
+    name: "NextJS Object Detection",
+    image: "/projects/ObjectDetection.png",
+    blurImage: "/projects/blur/airbnb-blur.webp",
+    description:
+      "NextJS Object Detection Using ShadCN, Tensorflow.js and Coco-ssd Model 🚀",
+    gradient: ["#000066", "#6699FF"],
+    url: "https://nextjs-object-detection.onrender.com/",
+    tech: ["nextjs", "tailwindcss", "tensorflow", "react"],
+  },
+  {
+    name: "CatStronaut",
+    image: "/projects/CatStronaut.png",
+    blurImage: "/projects/blur/tesla-blur.webp",
+    description: "A CatStronaut WebApp Using React and GraphQL 🚀",
+    gradient: ["#142D46", "#2E4964"],
+    url: "https://lift-off-client-demo.netlify.app/",
+    tech: ["react", "graphql", "typescript", "chakra-ui"],
+  },
+  {
+    name: "NodeJS Social Backend",
+    image: "/projects/Node.png",
+    blurImage: "/projects/blur/medium-blur.webp",
+    description: "A Complex Relational Backend  📰",
+    gradient: ["#12063b", "#09555c"],
+    url: "https://node-postgresql-q54m.onrender.com/docs/",
+    tech: ["expressjs", "nodejs", "postgresql", "prisma", "swagger"],
+  },
+  {
+    name: "Sharyng.ai",
+    image: "/projects/Sharyng.png",
+    blurImage: "/projects/blur/tesla-blur.webp",
+    description: "A React Native App to search image by face scan 🚀",
+    gradient: ["#e6220c", "#ffad5c"],
+    url: "/",
+    tech: ["react", "firebase", "javascript", "css"],
+  },
+  {
+    name: "Job Finder App",
+    image: "/projects/JobApp.jpg",
+    blurImage: "/projects/blur/airbnb-blur.webp",
+    description: "A React Native App to search jobs using RapidAPI 🚀",
+    gradient: ["#d8e547", "#56d388"],
+    url: "/",
+    tech: ["react", "typescript", "css", "styledcomponents"],
+  },
+];
+
+export const WORK_CONTENTS = {
+  GEEKSOFKOLACHI: [
+    {
+      title: "Geeks Of Kolachi",
+      description: "A company focused on innovative software solutions.",
+      content: (
+        <div className="h-full w-full flex items-center justify-center text-white px-4">
+          Revolutionizing software development, one line of code at a time.
+        </div>
+      ),
+    },
+    {
+      title: "Frontend Development",
+      description:
+        "As an Associate Software Engineer, I focus on frontend development using TypeScript, React, GraphQL, Next.js, and React Native. My specialization in clean and maintainable code ensures optimal user experiences. With expertise in TypeScript, I contribute to robust codebases and efficient data management using MongoDB.",
+      content: (
+        <div className="h-full w-full flex items-center justify-center text-white px-4">
+          Associate Software Engineer
+        </div>
+      ),
+    },
+    {
+      title: "Internship Experience",
+      description:
+        "During my internship, I engaged with a dynamic range of technologies, including TypeScript, Next.js, and React Native. I gained practical experience across multiple fronts and contributed to projects involving seamless user experiences spanning various platforms.",
+      content: (
+        <div className="h-full w-full flex items-center justify-center text-white px-4">
+          Intern
+        </div>
+      ),
+    },
+  ],
+
+  SHARYNG: [
+    {
+      title: "Sharyng.ai",
+      description:
+        "A platform aimed at enhancing user experience and functionality through innovative mobile applications.",
+      content: (
+        <div className="h-full w-full flex items-center justify-center text-white px-4">
+          Innovating user interactions, one app at a time.
+        </div>
+      ),
+    },
+    {
+      title: "Mobile Application Development",
+      description:
+        "As a Software Engineer (Contract) from October 2023 to March 2024, I developed a cross-platform mobile application using React Native. Key achievements include implementing seamless photo uploads, integrating basic face recognition for user identity verification, designing an integrated gallery feature for managing bulk photos, enabling file sharing for various types, and developing a QR code scanner feature.",
+      content: (
+        <div className="h-full w-full flex items-center justify-center text-white px-4">
+          Software Engineer – Contract
+        </div>
+      ),
+    },
+  ],
+};
+
+export const GTAG = "G-5HCTL2TJ5W";
